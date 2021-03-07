32e6ff233c34d6d29aed6fd643793cf02f3a4d9f
Brandon Maxwell <brandon.l.maxwell@gmail.com>
2021-03-07 09:14:12 -0600

    html structure completed. adding further JS functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 var timeDisplay = $('#currentDay');
-var timeBlock = $('.container')
+var hour = moment().format('h a');
+// var input = getElementsByClassName(".textInput");
+var timeBlock = $('.container');
+var saveIcon = document.getElementsByClassName('.material-icons');
 //----------------------------------------------------
 //shows current date & time
 //----------------------------------------------------
@@ -9,6 +12,24 @@ function displayTime() {
 }
 displayTime();
 setInterval(displayTime,1000);
-//----------------------------------------------------
 
 //----------------------------------------------------
+//save button click event
+//----------------------------------------------------
+// saveIcon.addEventListener("click", function (event) {
+//     var textBox = document.getElementById('input').value;
+// })
+
+//----------------------------------------------------
+//if states for past/present/future
+//----------------------------------------------------
+$('.textInput').each(function(){
+    var val = parseInt($(this).prop('id'));
+    if (val > hour) {
+        $(this).addClass("future");
+    }else if (val == hour) {
+        $(this).addClass("present");
+    }else if (val < hour) {
+        $(this).addClass("past")
+    }
+})
